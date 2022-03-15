60048cb10a02455160360d4633b347f8e4d71fe2
Eyal Paz <eyal@ox.security>
2022-03-15 19:47:09 +0200

    simple db connection

--- lesson1/app.py
+++ lesson1/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+import pandas as pd
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///users.db')
+
+
+user_database = []
 
 @app.route("/")
 def hello_world():
@@ -9,6 +15,22 @@ def hello_world():
 @app.route("/welcome", methods=["POST"])
 def welcome():
     return "Welcome"
+
+@app.route("/add-user", methods=["POST"])
+def add_user():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    new_user_df = pd.DataFrame.from_dict([{
+        'first_name': firstname,
+        'last_name': lastname,
+        'user_name': username,
+        'password': password
+    }])
+    conn = engine.connect()
+    new_user_df.to_sql('users', conn, if_exists='append', index=False)
+    return render_template('welcome.html', first_name=firstname)
 
 @app.route("/register")
 def register():
