7d292481d8c6a821a428675a6821c9cbaf93e74d
Eyal Paz <eyal@ox.security>
2022-05-03 18:01:04 +0300

    lesson6

---
+++ lesson6/netanya.js
@@ -0,0 +1,5 @@
+document.getElementById("loginButton").innerHTML = "submit"
+
+img = document.getElementsByClassName("img-fluid");
+console.log(img)
+img[0].setAttribute("src", "https://yedion.yvc.ac.il/info/images/CollegeLogo.png");
