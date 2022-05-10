8a0dbd3525f29b6e42a63a3d191bf6c6073f80e6
Eyal Paz <eyal@ox.security>
2022-05-10 20:19:40 +0300

    steal cookie

---
+++ lesson7/steal-cookies.js
@@ -0,0 +1,15 @@
+// solution by kareem yahia
+var getCookies = function(){
+  var pairs = document.cookie.split(";");
+  var cookies = {};
+  for (var i=0; i<pairs.length; i++){
+    var pair = pairs[i].split("=");
+    cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+  }
+  return cookies;
+}
+
+
+var myCookies = getCookies();
+
+console.log(myCookies);
